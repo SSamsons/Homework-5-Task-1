2b5509fe31f32f45d3e92a43454e87a896ca5dc9
Stanislav <people.ss@yandex.ru>
2023-03-20 23:22:55 +0300

    Repair commit

--- src/test/java/ru/netology/services/MonthsServiceParameterizedTest.java
+++ src/test/java/ru/netology/services/MonthsServiceParameterizedTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class MonthsServiceParameterizedTest {
 
     @ParameterizedTest
-    @CsvFileSource(files ="src/test/resources/month.csv")
-    public void shouldCalcMonth(int expected, int income, int expence, int trashold) {
+    @CsvFileSource(files ="src/test/resources/months.csv")
+    public void shouldCalcMonths(int expected, int income, int expence, int trashold) {
         MonthsService service = new MonthsService();
 
         int actual = service.calcMoney(income, expence, trashold);
